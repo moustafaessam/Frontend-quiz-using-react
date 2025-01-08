b731c2b947ad0e9d4916272144e931c684114930
Moustafa Essam <moustafaessam2212@gmail.com>
2025-01-08 05:36:02 +0200

    bug fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import data from "../assets/data.json";
 import { thunk } from "redux-thunk";
 
